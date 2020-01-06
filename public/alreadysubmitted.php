e6f092092466735e28e8742b3470b8de85d1ef48
Dhara Ghodasara <dharapatel61998@gmail.com>
2020-01-06 14:48:00 +0530

    commit chnages

---
+++ public/alreadysubmitted.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link href='https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700' rel='stylesheet' type='text/css'>
+	<style>
+
+
+		@import url(//cdnjs.cloudflare.com/ajax/libs/normalize/3.0.1/normalize.min.css);
+		@import url(//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css);
+
+	</style>
+	<link rel="stylesheet" href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css">
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+	<script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+</head>
+<body id="backgroundImage" >
+	
+	<header class="site-header" id="header">
+		<img src="/images/fitness5.png">
+		<p style="font-size: 80px; font-weight: 900;">Already Done !</p>
+	</header>
+
+	<div class="main-content">
+		<i class="fa fa-check-square-o main-content__checkmark" id="checkmark"></i>
+		<p class="main-content__body" data-lead-id="main-content-body">You Have Already Submitted a Form !!! <br> Kindly, Contact Administrator for Your Application</p>
+	</div>
+
+	<footer class="site-footer" id="footer">
+		<p class="site-footer__fineprint" id="fineprint"><strong>Copyright &copy; 2019-2020 <a href="#">WB Club</a>.</strong>| All Rights Reserved</p>
+	</footer>
+
+
+</body>
+</html>	
