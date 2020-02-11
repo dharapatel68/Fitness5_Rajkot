39163ccaf6e946880644c74e967d2ec33274433e
Dhara Ghodasara <dharapatel61998@gmail.com>
2020-02-11 18:09:14 +0530

    Update HRController.php

--- app/Http/Controllers/HRController.php
+++ app/Http/Controllers/HRController.php
@@ -531,7 +531,7 @@ class HRController extends Controller
 
 		$empid = $request->employeeid;
 
-		$account = EmployeeAccount::with('employeename')->where('employeeid', $empid)->get()->all();
+		$account = EmployeeAccount::with('employeename')->where('employeeid', $empid)->get()->paginate(10);
 		$employee = Employee::where('status', 1)->get()->all();
 
 
