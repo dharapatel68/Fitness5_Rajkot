a06d465c51354e0cb6bb92d6e84549716a6fbbed
Dhara Ghodasara <dharapatel61998@gmail.com>
2020-03-06 10:54:47 +0530

    commit changes

--- cronjob/packageexpirecron.php
+++ cronjob/packageexpirecron.php
@@ -1,31 +1,31 @@
 <?php
 
-  // $conn= mysqli_connect("localhost", "gym_weybee", "gymweybee@123", "gym_weybee"); 
-  //  $conn= mysqli_connect("localhost", "admin_fitness5mumbai", "fitness5mumbai@123","admin_fitness5mumbai");
-  // $query='SELECT * FROM memberpackages left join member on memberpackages.userid = member.userid WHERE member.status = 1';
-  // $result=mysqli_query($conn,$query);
+  //$conn= mysqli_connect("localhost", "admin_fitness5", "fitness5@123", "admin_fitness5"); 
+  $query='select * from memberpackages';
+  $result=mysqli_query($conn,$query);
 
-  //    $today = date("Y-m-d"); 
-  //      $enddate= "SELECT * FROM memberpackages left join member on memberpackages.userid = member.userid WHERE member.status = 1";
-  //    $result=mysqli_query($conn,$enddate);
-  //    $data=array();
-  //    while($row=mysqli_fetch_assoc($result)){
-  //      $data[]=$row;
-  //    }
+     $today = date("Y-m-d"); 
+       $enddate= "select * from memberpackages where status=1";
+     $result=mysqli_query($conn,$enddate);
+     $data=array();
+     while($row=mysqli_fetch_assoc($result)){
+       $data[]=$row;
+     }
     
-  //   for($i=0;$i<count($data);$i++)
-  //   {
-  //     if($data[$i]['expiredate'] < $today){
+    for($i=0;$i<count($data);$i++)
+    {
+      if($data[$i]['expiredate'] < $today){
          
-  //       $sql= "UPDATE memberpackages SET status='0' where expiredate ='".$data[$i]['expiredate']."'";
-  //           mysqli_query($conn,$sql);
+        $sql= "UPDATE memberpackages SET status='0' where expiredate ='".$data[$i]['expiredate']."'";
+            mysqli_query($conn,$sql);
      
-  //          }
-  //   }
-  
-  //   echo "Package Expire Successfull";
+           }
+    }
+       
+    
+  echo "Package Expire Successfull";
 
-   $conn= mysqli_connect("localhost", "root", "","gms_new");
+
   $query='SELECT *,MAX(expiredate) as maxdate FROM memberpackages where status = 1 GROUP BY userid';
   $result=mysqli_query($conn,$query);
 
@@ -49,5 +49,28 @@
   
     echo "Member Expire Successfull";
 
+
+
+    /************************************************* */
+    $query='SELECT * FROM memberpackages where status = 1 GROUP BY userid';
+    $result=mysqli_query($conn,$query);
+  
+        
+       $data=array();
+       while($row=mysqli_fetch_assoc($result)){
+         $data[]=$row;
+       }
+      
+      for($i=0;$i<count($data);$i++)
+      {
+        
+           
+          $sql= "UPDATE member SET status='1' where userid ='".$data[$i]['userid']."'";
+              mysqli_query($conn,$sql);
+       
+             
+      }
+    
+      echo "Member Activeted Successfull";
 
 ?>
